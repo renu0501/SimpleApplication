48be11464dac22bd4439dce391b94e3aa220cb84
renu0501 <renu0501joshi@gmail.com>
2022-09-13 01:19:42 +0530

    initial commit

---
+++ src/main/java/com/renu/controller/HelloController.java
@@ -0,0 +1,8 @@
+package com.renu.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+}
---
+++ src/test/java/com/renu/SimpleApplication/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renu.SimpleApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
